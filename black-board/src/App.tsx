4c501d406a0220847baac5f06f3a15949fc03bfa
jihye5081 <ekftn90@gmail.com>
2024-11-05 23:40:45 +0900

    feat: 프로젝트 기본 설정 및 blackboard 컴포넌트 init

---
+++ black-board/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Blackboard
+ from './page/study/Blackboard';
+
+function App() {
+  return (
+    <Blackboard />
+  );
+}
+
+export default App;
---
+++ black-board/src/page/study/Blackboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Blackboard() {
+  return (
+    <div>
+      정윤미 gpt
+    </div>
+  )
+}
