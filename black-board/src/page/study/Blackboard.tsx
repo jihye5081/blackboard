10a4085d406f760ecc354dcb4f3eac7fecd8728b
jihye5081 <ekftn90@gmail.com>
2024-11-07 22:36:22 +0900

    feat: 칠판 UI 작성

--- black-board/src/page/study/Blackboard.tsx
+++ black-board/src/page/study/Blackboard.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function Blackboard() {
   return (
-    <div>
-      정윤미 gpt
-    </div>
+    <Wrapper>
+        <Header> 디지털 칠판 </Header>
+        <Section>
+          <BoardView></BoardView>
+          <BoardInput placeholder='입력해주세요.'/>
+        </Section>
+    </Wrapper>
+ 
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction : column;
+`;
+
+const Header = styled.h1`
+  display: flex;
+  flex-direction: column;
+  font-size: 76px;
+  font-weight: 400;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const Section = styled.section`
+display:flex;
+flex-direction:column;
+align-items: center;
+justify-content: flex-start;
+`;
+
+const BoardView = styled.article`
+width: 800px;
+height: 500px;
+border: 1px solid black;
+background-color: #3F7F50;
+border-bottom: none;
+`;
+
+const BoardInput = styled.input`
+width: 796px;
+height: 32px;
+border: 1px solid black;
+text-align: center;
+`;
